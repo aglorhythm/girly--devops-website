90a8ab40d0ee563706efa7ff78637e4d56f97ac7
Olga Amitousa-Mankoy <olga.mankoy@gmail.com>
2024-06-06 15:05:58 +0200

    Add: Tofu - security module

---
+++ tofu/modules/security/variables.tf
@@ -0,0 +1,45 @@
+# ===================================
+# Author: @aglorhythm / girlysheet.cloud ✿
+# Description: Here we define our variables. They can be used dynamically !
+# ===================================
+
+# VPC ID where the security group will be created
+variable "vpc_id" {
+  type        = string
+  description = "The ID of the VPC where the security group will be created."
+  default     = "vpc-0ed36a10dfbbffe20"
+}
+
+# Security group
+variable "sg" {
+  type        = string
+  description = "List of security groups."
+  default     = "girlysheet_front_sg" # Focus on Paris & London 
+}
+
+# CIDR blocks to allow SSH access
+variable "ssh_access_cidr" {
+  type        = list(string)
+  description = "CIDR blocks that is allowed SSH access to the instances."
+  default     = ["0.0.0.0/0"]  
+}
+
+# CIDR blocks to allow HTTP access
+variable "http_access_cidr" {
+  type        = list(string)
+  description = "CIDR blocks that is allowed for HTTP access to the instances."
+  default     = ["0.0.0.0/0"]  
+}
+
+# CIDR blocks to allow HTTPS access
+variable "https_access_cidr" {
+  type        = list(string)
+  description = "CIDR blocks that is allowed for HTTPS access to the instances."
+  default     = ["0.0.0.0/0"]  
+}
+
+# SSH acces
+variable "ssh_port" {
+  type        = string
+  description = "Port for ssh"
+}
